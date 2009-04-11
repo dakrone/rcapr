742ca7e26feeb9ee27d93c324b4ec41699f243f0
dakrone <lee-at-writequit.org>
2009-04-11 16:54:54 -0600

    still researching how to upload a file with ruby

--- lib/rcapr.rb
+++ lib/rcapr.rb
@@ -4,6 +4,7 @@ require 'rubygems'
 require 'httparty'
 require 'json'
 require 'open-uri'
+require 'pp'
 
 class Rcapr
   def initialize(username, password)
@@ -13,9 +14,11 @@ class Rcapr
   end
 
   def upload(filename)
+    return false unless File.exist?(filename)
     STDERR.puts "[+] uploading #{filename}"
-    result = JSON.parse(@api.upload(filename))
     #result = @api.upload(filename)
+    pp result
+    #result = JSON.parse(@api.upload(filename))
     # '{ okay: true, id: "uploaded-pcap" }'
     return result[:okay]
   end
@@ -39,10 +42,17 @@ class RcaprAPI
   # Given a file, upload the file
   def upload(file)
     begin
-      options = { :basic_auth => @auth, :file => "@#{file}" }
-      result = self.class.post('/upload', options)
-      STDERR.puts "result: #{result}"
-      return result
+      # TODO: find a ruby way of uploading a file using basic auth
+      #options = { :basic_auth => @auth, :file => "@#{file}" }
+      #mp = Multipart.new(filename)
+      #mp.post("http://www.pcapr.net/api/upload")
+      #result = self.class.post('/upload', options)
+      #STDERR.puts "result: #{result}"
+      #return result
+      
+      result = `curl --basic -u #{@auth[:username]}:#{@auth[:password]} -F file=@#{file} -F description="testing the API" -F tags="api" http://www.pcapr.net/api/upload`
+      return nil if result.nil?
+      return result.to_json
     rescue
       return {:okay => false}.to_json
     end
